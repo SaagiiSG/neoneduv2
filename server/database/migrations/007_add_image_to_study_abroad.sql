c5d797f9b0d8f5d6ac215e941a7e1565f4385a55
Saranochir Sereglen <saranochirsereglen@Saranochirs-Laptop.local>
2025-09-29 15:14:13 +0800

    Clean up codebase: remove unused scripts, docs, components, client folder, server folder

--- server/database/migrations/007_add_image_to_study_abroad.sql
+++
@@ -1,7 +0,0 @@
--- Add image column to study_abroad table
-ALTER TABLE study_abroad 
-ADD COLUMN IF NOT EXISTS image TEXT;
-
--- Add comment to the column
-COMMENT ON COLUMN study_abroad.image IS 'URL of the study abroad program image stored in Cloudinary';
-
